8633e9475e54868c270d1fb31237d50e3fa58fa3
Dan <danii-benitez@hotmail.com>
2023-01-26 19:17:22 -0300

    [fix] init dummy cert

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(sodanclothes.org www.sodanclothes.org)
+domains=(sodanclothes.com www.sodanclothes.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="danii-benitez@hotmail.com" # Adding a valid address is strongly recommended
